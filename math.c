c750baba6f4585bd2537de0b9f4289ddaa709021
MrCodeBin <saveencodes@gmal.com>
2022-04-05 17:06:50 +0545

    Math.h

---
+++ math.c
@@ -0,0 +1,49 @@
+//Headerfile//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//intmain
+int main()
+{
+	printf("The abs (-100) =%d\n",abs(-10.7));// abs vnyaa - sign hatne ani decimal ni hatne//int sadhai/////////////stdlib 
+	   printf("fmax of(50,45.8)=%.2f\n",fmax(50,45.8)); //kun value maximum hune check garne//
+	    printf("fmin of (50,45.8)=%.2f\n",fmin(50,45.8));//kun value minimum chcek garne//
+	       printf("exp value of (2) =%.2f\n",exp(2)); //expotential value power 2// eko power 2//
+	         printf("log of (34)=%.2f\n",log(34));
+	           printf("Square root of (25)= %.2f\n",sqrt(25));
+	             printf("Cube root of (125)=%.2f\n",cbrt(125));
+	                printf("Power of (4 to the power 5)=%.2lf",pow(4,5));
+	                 printf("Sin(90)=%.2lf\n",sin(90));
+	                   printf("Sin inverse of (90)=%.2lf\n",asin(90));//inverse sin/cos/tan...
+	                      printf("Cos of (45)=%.2lf\n",cos(45));
+	                      printf("Ceiling of the int=%.2lf\n",ceil(3.66));//ceiling ? mathi ko integer dene
+	                        printf("Floor of the int=%.2lf\n",floor(3.66));//floor ?talako integer dene
+	                          printf("Round of the number=%.2lf\n",round(3.5));//roundoff gardene
+	                             printf("Truncate of the number=%.2lf",trunc(3.4));//yesley chai decimal paxadi ko value lai hatai dene*88***8
+	                          
+	                           
+	                        
+	                   
+	                   
+	                   
+	                   
+	             
+	             
+	             
+	    
+	       
+	   
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
