7304d82d2ec3360472e7f97893aee6e5795d0cb6
Sabin Dahal <saveencodes@gmail.com>
2022-06-04 14:28:06 +0545

    display prime number

--- practice2.0.c
+++ practice2.0.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
-
-int main()
+int main ()
 {
-    int num,i,count=0;
+    int num,i,j,count;
+
     printf("Enter a number : ");
     scanf("%d",&num);
-
-    for (i=2;i<=num;i++)
+    printf("%d",1);
+    for (i=1; i<=num; i++)
     {
-        if(num%i ==0)
+        count =0;
+        for(j=1; j<=i; j++)
         {
-            count ++;
-        }
-    }
+            if(i%j==0)
+            {
+                count ++;
 
-    if (count ==1)
+            }
+        }
+    if (count ==2)
     {
-        printf("Prime number");
+        printf("%d",i);
     }
-    else if (num == 1)
-    {
-        printf("Prime number");
-    }
-    else
-    {
-        printf("Not Prime number");
     }
     return 0;
-
 }
